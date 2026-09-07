12a3205c120029e6e73e86bac254a5a5a88616db
asudula <sudula@unc.edu>
2026-09-07 18:14:09 -0400

    Refactor FuelGenerator constructor and methods

--- src/main/java/edu/unc/comp210/a02mothership/FuelGenerator.java
+++ src/main/java/edu/unc/comp210/a02mothership/FuelGenerator.java
@@ -3,27 +3,27 @@ package edu.unc.comp210.a02mothership;
 public class FuelGenerator extends APowerGenerator {
     private int fuel;
 
-    public FuelGenerator(int fuel, String name) {
-        super(name);
+    public FuelGenerator(int fuel) {
+        super("FuelGenerator");
         this.fuel = fuel;
     }
 
     @Override
     public void statusReport(String moduleStatus, boolean isSuccessful) {
-        System.out.println("FuelGenerator: "+ this.fuel +" units of fuel remaining.");
+        System.out.println("FuelGenerator: " + this.fuel + " units of fuel remaining.");
         super.statusReport(moduleStatus, isSuccessful);
     }
 
     @Override
-    public int generatePower(){
+    public int generatePower() {
         if (fuel >= 10) {
             fuel -= 10;
             return 10;
         }
         else {
+            int remainingFuel = fuel;
             fuel = 0;
-            return fuel;
+            return remainingFuel;
         }
     }
 }
-
